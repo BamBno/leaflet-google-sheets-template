9437e6481717a89659f3f1cf98258234646332b6
Brendon Norris <norris.brendonw@gmail.com>
2017-10-16 22:27:40 +0000

    Updated JSON URL

--- geojson.php
+++ geojson.php
@@ -1,7 +1,7 @@
 <?php
 // kudos to http://stackoverflow.com/a/18106727/1778785 for snippet of PHP to read Google spreadsheet as CSV
 
-$googleSpreadsheetUrl = "https://docs.google.com/spreadsheet/pub?key=10eNXFh6mzFtii7B2PW90jmHtrQLJlRCrf3kkHU0HIH8&single=true&gid=0&output=csv";
+$googleSpreadsheetUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vT3wiR1k0QM30Zq8dC4hjvQrcVoyOOCWO-etoVg__N9VL7KdHJvxgqEqxbW-Nwan29Q_X3N8zc8dcho/pub?output=csv";
 
 $rowCount = 0;
 $features = array();
